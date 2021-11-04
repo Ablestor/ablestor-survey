057103165e4c68589307195b7fad7878bab5dcd4
im-ian <38205068+im-ian@users.noreply.github.com>
2021-11-04 13:32:45 +0900

    Fix: 불필요한 import 삭제

--- src/Viewer/index.tsx
+++ src/Viewer/index.tsx
@@ -1,4 +1,4 @@
-import { useState, useEffect, useCallback } from 'react';
+import { useState, useEffect } from 'react';
 import update from 'immutability-helper';
 import { Row, SurveyContainer } from '../components/Section';
 import { ISurveyViewer } from '../@types/viewer';
