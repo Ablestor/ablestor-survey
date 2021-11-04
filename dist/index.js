06f669237c3e80e4dae755af30af650587d2cc39
im-ian <38205068+im-ian@users.noreply.github.com>
2021-11-04 17:53:28 +0900

    Revert "Fix: export"
    This reverts commit 269aa905c74dd47fdbe370e404cc2697799b5a69.

--- dist/index.js
+++ dist/index.js
@@ -1,17 +1,7 @@
-export * as BlockType from './@types/block';
-export * as EditorType from './@types/editor';
-export * as ViewerType from './@types/viewer';
-export * as Editor from './Editor';
-export * as Viewer from './Viewer';
-export * as Converter from './helpers/converter';
-export * as Generator from './helpers/generator';
-
-export {
-  BlockType,
-  EditorType,
-  ViewerType,
-  Editor,
-  Viewer,
-  Converter,
-  Generator,
-};
+export * from './@types/block';
+export * from './@types/editor';
+export * from './@types/viewer';
+export * from './Editor';
+export * from './Viewer';
+export * from './helpers/converter';
+export * from './helpers/generator';
