188f19437fcdb6a162c8ab3b1b186013e934d7ee
Junseo Oh <insung9546@gmail.com>
2022-03-05 02:43:29 +0900

    fix: demo

--- demo/src/App.tsx
+++ demo/src/App.tsx
@@ -7,15 +7,20 @@ const App = () => {
 
   return (
     <div style={{ display: 'flex' }}>
-      <div style={{ flex: 1, justifyContent: 'center' }}>
-        <Editor onSubmit={setSurveyContent} />
+      <div style={{ flex: 1, justifyContent: 'center', padding: 16 }}>
+        <Editor
+          submitButtonOptions={{ text: '설문 만들기', visible: true }}
+          onSubmit={setSurveyContent}
+        />
       </div>
-      <div style={{ flex: 1 }}>
+      <div style={{ flex: 1, padding: 16 }}>
         {surveyContent && (
           <Viewer
+            submitButtonOptions={{ text: '설문 제출 하기', visible: true }}
             survey={surveyContent}
             onSubmit={result => {
               console.log(result);
+              alert(JSON.stringify(result));
             }}
           />
         )}
